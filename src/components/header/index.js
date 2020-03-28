375bcb18fc9eeef31ce9e54d04a602da48298cff
Abilio Bittencourt Junior <abilio.jr@live.com>
2020-03-28 13:09:40 -0300

    Removendo bootstrap

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,34 +3,11 @@ import React from 'react';
 import logo from '../../logo.svg'
 
 const Header = () => {
-    return <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container">
-      <a className="navbar-brand" href="#">
-            <img src={logo} alt="" />
-          </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Item
-                  <span className="sr-only">(current)</span>
-                </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Item</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Item</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Item</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <img src={logo} alt="" />
+    </nav>
+  )
 }
 
 export default Header;
