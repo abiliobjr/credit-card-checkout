9bc7650c2c01a6ec92a01979854017f28202934a
Abilio Bittencourt Junior <abilio.jr@live.com>
2020-03-28 12:08:50 -0300

    Criando header temporário

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import Header from './components/header';
+
+
 function App() {
   return (
+    [
+    <Header />,
     <div className="container">
       <div className="row">
         <div className="col-md-12">
           <h1>Começando aplicação React.js</h1>
         </div>
       </div>
-    </div>
+    </div>]
   );
 }
 
