8d587ed57a319ef75d20935dd8eb7713a19f36ef
Abilio Bittencourt Junior <abilio.jr@live.com>
2020-03-28 22:04:47 -0300

    Incluindo componente do cartão de crédito e fazendo lado esquerdo do layout

--- src/pages/paymentPage/index.js
+++ src/pages/paymentPage/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import Steps from '../../components/steps';
+import Cards from 'react-credit-cards';
 
+import Steps from '../../components/steps';
+import back from '../../images/back.svg';
+import CreditCard from '../../images/credit-card.svg';
 
 class PaymentPage extends Component {
-
     render(){
         return (
             <div className="container flex-container">
                 <div className="flex-container content-box" id="payment">
                     <div className="payment-method-box">
-                        <h1>metodo de pagamento</h1>
+                        <div className="change-payment-method-box">
+                            <a href="#">
+                                <img src={back} alt=""/>
+                                Alterar forma de pagamento
+                            </a>
+                        </div>
+                        <div className="flex-container">
+                            <img src={CreditCard} alt=""/>
+                            <h1>Adicione um novo cartão de crédito</h1>
+                        </div>
+                        <Cards
+                            cvc={123}
+                            expiry={'20/20'}
+                            focused={''}
+                            name={'this.state.name'}
+                            number={'5149 4505 6231 6542'}
+                        />
                     </div>
                     <div className="payment-form">
                         <Steps />
